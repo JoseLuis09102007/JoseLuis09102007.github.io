8b3cd13c2b2a03a613093a60a7a496078a5078de
JoseLuis09102007 <71952988+JoseLuis09102007@users.noreply.github.com>
2020-10-07 16:16:06 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,14 @@
 alert("Estas ingresando a la pagina web de Jose Luis");
+let name = prompt("Ingresa tu primer nombre");
+if (name === "Alexis" || name === "alexis") {
+  alert("Hola amigo de Jose Luis");
+} else if (
+  name === "Melany" ||
+  name == "melany" ||
+  name === "melani" ||
+  name == "Melany"
+) {
+  alert("Hola Esposa de Jungkook");
+} else {
+  alert("Al parecer no te conozco");
+}
